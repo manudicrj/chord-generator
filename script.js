6cf47d05f184cbac09c356dfdb4bd65991143249
manudicrj <manuel.dicriscito@jump-it.it>
2021-07-24 18:33:57 +0200

    Bug sistemati, nuovi accordi

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+function isPrimitive(test) {
+    return test !== Object(test);
+}
+
+Array.prototype.equals = function(b) {
+    var a = this;
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+Array.prototype.copy = function() {
+    var that = this;
+    var b = [];
+    for(var t of that) {
+        b.push(t);
+    }
+    return b;
+}
+
 var midi = null;
 onMIDISuccess = (access) => {
     console.log("MIDI Ready!");
@@ -25,8 +50,8 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 function randomBoolean(trueProb) {
-    if(!trueProb) trueProb = 50;
-    return random(1, 100)<trueProb;
+    if (!trueProb) trueProb = 50;
+    return random(1, 100) < trueProb;
 }
 Array.prototype.getRandom = function () {
     var array = this;
@@ -40,7 +65,7 @@ const types = [
             short: "|M|Δ",
             long: "maj"
         },
-        notes: [0,4,7],
+        notes: [0, 4, 7],
         prob: 50,
     }, {
         name: "Minor triad",
@@ -48,7 +73,7 @@ const types = [
             short: "m|-",
             long: "min"
         },
-        notes: [0,3,7],
+        notes: [0, 3, 7],
         prob: 50
     }, {
         name: "Augmented triad",
@@ -56,122 +81,136 @@ const types = [
             short: "+",
             long: "aug"
         },
-        notes: [0,4,8]
+        notes: [0, 4, 8]
     }, {
         name: "Diminished triad",
         text: {
             short: "°",
             long: "dim"
         },
-        notes: [0,3,6]
+        notes: [0, 3, 6]
     }, { // seventh chords
         name: "Dominant seventh",
         text: {
             short: "<sup>7</sup>",
             long: "<sup>7</sup>"
         },
-        notes: [0,4,7,10]
+        notes: [0, 4, 7, 10]
     }, {
         name: "Major seventh",
         text: {
             short: "M<sup>7</sup>|<sup>maj7</sup>|Δ<sup>7</sup>",
             long: "maj<sup>7</sup>"
         },
-        notes: [0,4,7,11]
+        notes: [0, 4, 7, 11]
     }, {
         name: "Minor seventh",
         text: {
             short: "m<sup>7</sup>|-<sup>7</sup>",
             long: "min<sup>7</sup>"
         },
-        notes: [0,3,7,10]
+        notes: [0, 3, 7, 10]
     }, { // Ninth chords
         name: "Major ninth",
         text: {
             short: "M<sup>9</sup>|<sup>Δ9</sup>",
             long: "maj<sup>9</sup>"
         },
-        notes: [0,4,7,11,2]
+        notes: [0, 4, 7, 11, 2]
     }, {
         name: "Dominant ninth",
         text: {
             short: "<sup>9</sup>",
             long: "<sup>9</sup>"
         },
-        notes: [0,4,7,10,2]
+        notes: [0, 4, 7, 10, 2]
     }, {
         name: "Dominant minor ninth",
         text: {
             short: "<sup>7♭9</sup>",
             long: "<sup>7♭9</sup>"
         },
-        notes: [0,4,7,10,1]
+        notes: [0, 4, 7, 10, 1]
     }, {
         name: "Minor ninth",
         text: {
             short: "m<sup>9</sup>|-<sup>9</sup>",
             long: "min<sup>9</sup>"
         },
-        notes: [0,3,7,10,2]
+        notes: [0, 3, 7, 10, 2]
     }, { // eleventh chords
         name: "Eleventh",
         text: {
             short: "<sup>11</sup>",
             long: "<sup>11</sup>"
         },
-        notes: [0,4,7,10,5]
+        notes: [0, 4, 7, 10, 5]
     }, {
         name: "Major eleventh",
         text: {
             short: "M<sup>11</sup>",
             long: "maj<sup>11</sup>"
         },
-        notes: [0,4,7,11,2,5]
+        notes: [0, 4, 7, 11, 2, 5]
     }, {
         name: "Minor eleventh",
         text: {
             short: "m<sup>11</sup>|-<sup>11</sup>",
             long: "min<sup>11</sup>"
         },
-        notes: [0,3,7,10,2,5]
+        notes: [0, 3, 7, 10, 2, 5]
     }, { // Thirteenth chords
         name: "Major thirteenth",
         text: {
             short: "M<sup>13</sup>|Δ<sup>13</sup>",
             long: "maj<sup>13</sup>"
         },
-        notes: [0,4,7,11,2,5,9]
+        notes: [0, 4, 7, 11, 2, 5, 9]
     }, {
         name: "Thirteenth",
         text: {
             short: "<sup>13</sup>",
             long: "<sup>13</sup>"
         },
-        notes: [0,4,7,10,2,5,9]
+        notes: [0, 4, 7, 10, 2, 5, 9]
     }, {
         name: "Minor thirteenth",
         text: {
             short: "m<sup>13</sup>|-<sup>13</sup>",
             long: "min<sup>13</sup>"
         },
-        notes: [0,3,7,11,2,5,9]
+        notes: [0, 3, 7, 11, 2, 5, 9]
     }, { // added tone chords
         name: "6/9",
         text: {
             short: "<sup>6/9</sup>",
             long: "<sup>6/9</sup>"
         },
-        notes: [0,4,7,9,2]
+        notes: [0, 4, 7, 9, 2]
+    }, {
+        name: "Major sixth",
+        text: {
+            short: "<sup>6</sup>",
+            long: "<sup>6</sup>"
+        },
+        notes: [0, 4, 7, 9]
+    }, {
+        name: "Minor sixth",
+        text: {
+            short: "m<sup>6</sup>",
+            long: "minor<sup>6</sup>"
+        },
+        notes: [0, 3, 7, 9]
     }
 ]
 const sus = [{
     name: "Suspended fourth",
     text: "<sup>sus4</sup>|<sup>sus</sup>",
-    notes: [5]
+    note: 5
 }, {
     name: "Suspended second",
     text: "<sup>sus2</sup>",
-    notes: [2]
+    note: 2
 }];
 const adds = [{
     text: "2|9",
@@ -234,54 +273,82 @@ function pickOne(s) {
 
 
 
+
 var chord = {};
 function generateChord() {
 
     var chord = {};
-    chord.type = types[random(0, types.length-1)];
+    chord.type = types[random(0, types.length - 1)];
     chord.offset = random(0, 11);
-    chord.notes = chord.type.notes;
+    chord.notes = chord.type.notes.copy();
     chord.add = null;
     chord.sus = null;
-    if(randomBoolean(10)) {
-        chord.add = adds[random(0, adds.length-1)];
-        if(chord.notes.includes(chord.add.note)) {
-            console.log(chord.add, chord.notes);
+    if (randomBoolean(10)) {
+        chord.add = adds[random(0, adds.length - 1)];
+        if (chord.notes.includes(chord.add.note)) {
             chord.add = null;
         } else {
             chord.notes.push(chord.add.note);
+            for(var type of types) {
+                if (chord.notes.equals(type.notes)) {
+                    chord.type = type;
+                    chord.notes = chord.type.notes;
+                    chord.add = null;
+                    break;
+                }
+            }
         }
-    } else if(randomBoolean(10)) {
-        chord.sus = sus[random(0, sus.length-1)];
-        if(chord.sus.notes.every(n => chord.notes.includes(n))) {
-            console.log(chord.sus, chord.notes);
+
+        types.map((type, i) => {
+
+        });
+    } else if (randomBoolean(10)) {
+        chord.sus = sus.getRandom();
+        if(chord.notes.includes(chord.sus.note)) {
             chord.sus = null;
         } else {
-            chord.notes.concat(chord.sus.notes);
+            chord.notes.push(chord.sus.note);
+            for(var type of types) {
+                if(chord.notes.equals(type.notes)) {
+                    chord.type = type;
+                    chord.notes = chord.type.notes;
+                    chord.sus = null;
+                    break;
+                }
+            }
         }
     }
     var noteText = pickOne(notes[chord.offset]);
-    
 
-    console.log($("#notation").value);
-    var notation = $("#notation").value==='Short' ? pickOne(chord.type.text.short) : chord.type.text.long;
-    if(chord.add) {
-        notation += "<sup>add"+pickOne(chord.add.text)+"</sup>";
+    var notation = $("#notation").value === 'Short' ? pickOne(chord.type.text.short) : chord.type.text.long;
+    if (chord.add) {
+        notation += "<sup>add" + pickOne(chord.add.text) + "</sup>";
     }
-    if(chord.sus) {
+    if (chord.sus) {
         notation += pickOne(chord.sus.text);
     }
     chord.text = noteText + notation;
+    
+    /* // print
     console.error("-----");
-    for(var n of chord.notes) {
-        console.log(notes[(n+chord.offset)%12]);
+    for (var n of chord.notes) {
+        console.log(notes[(n + chord.offset) % 12]);
     }
+    */
 
     return chord;
 }
 
 var chordEl = $("#chord");
 chordEl.onclick = () => {
+    /* JESUS
+    var i = 0;
+    do {
+        chord = generateChord();
+        i++;
+    } while(chord.text != "G<sup>sus</sup>");
+    console.log("Generated " + i + " times");
+    */
     chord = generateChord();
     chordEl.innerHTML = chord.text;
 }
@@ -300,11 +367,12 @@ function listInputsAndOutputs(midiAccess) {
 
 var waitForKeysUp = false;
 var notCorrect = false;
+
 function onMIDIMessage(event) {
     var str = "MIDI message received at timestamp " + event.timestamp + "[" + event.data.length + " bytes]: ";
     var key = event.data[1];
     var down = event.data[0] == 144;
-    console.log(event.data);
+    
     if (down) {
         keyboard.push(key);
         var played = true;
@@ -313,7 +381,6 @@ function onMIDIMessage(event) {
             for (var k of keyboard) {
                 let key = (k) % 12;
                 let note = (n + chord.offset) % 12;
-                console.log(key, note);
                 if (key == note) {
                     notFound = false;
                     break;
@@ -331,14 +398,17 @@ function onMIDIMessage(event) {
 
     } else {
         keyboard = keyboard.filter(x => x != key);
-        if(!keyboard.length) {
+        if (!keyboard.length) {
             chordEl.style.color = '#696969';
             notCorrect = false;
-            if(waitForKeysUp) {
+            if (waitForKeysUp) {
                 waitForKeysUp = false;
                 chordEl.click();
-            } 
+            }
         }
+    }
+    if(event.data[0]==176) {
+        $("#chord").style.fontSize = event.data[2]*2+"px";
     }
 
 
